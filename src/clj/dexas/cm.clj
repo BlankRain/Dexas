350b10d29beb844b3d05d640b3ef5f2b63af77aa
GaiBang <3100247085@qq.com>
2017-03-01 22:45:19 +0800

    add core manager

---
+++ src/clj/dexas/cm.clj
@@ -0,0 +1,15 @@
+;;;; Core Manager
+(ns dexas.cm
+    (:gen-class))
+
+(deftype Customer)
+;;;; 从参数生成客户对象
+(defn request->Customer [req])
+;;;; 注册客户信息
+(defn registCustomer [customer])
+;;;; 生成相关DockerFile
+(defn buildCustomerDockerFile [customer])
+;;;; 开辟资源,运行镜像
+(defn rundockerForcustomer [customer  docker-file])
+
+
