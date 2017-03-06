6cc1c346dd833634ab1812a94f8578000157d7b6
blankrain <731014656@qq.com>
2017-03-06 05:39:25 -0800

    change to port 80

--- start.sh
+++ start.sh
@@ -8,3 +8,11 @@ PATH=/usr/local/openresty/nginx/sbin:$PATH
 export PATH
 
 nginx -p `pwd`/ -c conf/nginx.conf
+
+or if you need sudo in linux
+
+
+sudo /usr/local/openresty/nginx/sbin/nginx -p `pwd` -c conf/dexas.conf
+sudo /usr/local/openresty/nginx/sbin/nginx -p `pwd` -c conf/dexas.conf -s stop
+sudo /usr/local/openresty/nginx/sbin/nginx -p `pwd` -c conf/dexas.conf -s reload
+
