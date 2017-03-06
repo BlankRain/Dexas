c5520188c2e9e4cbfa8f8c69e7baeb9a9c605cb5
blankrain <731014656@qq.com>
2017-03-06 05:07:38 -0800

    add views

--- lualu/index.lua
+++ lualu/index.lua
@@ -1,1 +1,21 @@
-ngx.say("hello world")
+local template = require "resty.template"
+--[====[
+
+template.render([[
+<!DOCTYPE html>
+<html>
+<body>
+  <h1>{{message}}</h1>
+  </body>
+  </html>]], { message = "Hello, World!" })
+
+]====]
+
+local template = require "resty.template"
+template.render("index.view", {
+  title   = "Testing lua-resty-template",
+    message = "Hello, World!",
+      names   = { "James", "Jack", "Anne" },
+        jquery  = '<script src="../../../js/node_modules/jquery/dist/jquery.min.js"></script>'
+	})
+ngx.say("nice job~!")
