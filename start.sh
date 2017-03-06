293cfd7b8c978823f8127811eece57312bb10a91
blankrain <731014656@qq.com>
2017-03-06 05:27:32 -0800

    change ignore file

--- start.sh
+++ start.sh
@@ -1,3 +1,7 @@
+sudo service docker restart
+sudo service redis-server restart
+docker run -dit --env app=felix --env redisHost=172.17.0.1 --hostname nicejob dexas:demo
+docker run -dit --env app=felicia --env redisHost=172.17.0.1 --hostname goodboy dexas:demo
 
 
 PATH=/usr/local/openresty/nginx/sbin:$PATH
